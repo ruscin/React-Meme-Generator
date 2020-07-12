0efe540df98a8cffe0f5637d87822f5f67584e4a
Kacper Ruściński <ruscin13@gmail.com>
2020-07-12 12:55:10 +0200

    Base that I will work on

---
+++ meme-generator/src/Header.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import './Header.css';
+
+class Header extends Component {
+  // constructor(props){
+    // super(props);
+    // this.state = {};
+  // }
+
+  // componentWillMount(){}
+  // componentDidMount(){}
+  // componentWillUnmount(){}
+
+  // componentWillReceiveProps(){}
+  // shouldComponentUpdate(){}
+  // componentWillUpdate(){}
+  // componentDidUpdate(){}
+
+  render() {
+    return (
+      <div>tu będzie header</div>
+    );
+  }
+}
+
+export default Header;
---
+++ meme-generator/src/MemeGenerator.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import './MemeGenerator.css';
+
+class MemeGenerator extends Component {
+  // constructor(props){
+    // super(props);
+    // this.state = {};
+  // }
+
+  // componentWillMount(){}
+  // componentDidMount(){}
+  // componentWillUnmount(){}
+
+  // componentWillReceiveProps(){}
+  // shouldComponentUpdate(){}
+  // componentWillUpdate(){}
+  // componentDidUpdate(){}
+
+  render() {
+    return (
+      <div>tu będzie meme</div>
+    );
+  }
+}
+
+export default MemeGenerator;
